9380b9c203d69a986add53a7bb007133c5146849
ljh <leejh95@nate.com>
2021-06-04 23:05:50 +0900

    Cache posts

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,23 +5,28 @@ import matter from 'gray-matter';
 export default (req, res) => {
   let posts;
 
-  const files = fs.readdirSync(path.join('posts'));
+  if (process.env.NODE_ENV === 'production') {
+    // Fetch from cache
+    posts = require('../../cache/data').posts;
+  } else {
+    const files = fs.readdirSync(path.join('posts'));
 
-  posts = files.map((filename) => {
-    const slug = filename.replace('.md', '');
+    posts = files.map((filename) => {
+      const slug = filename.replace('.md', '');
 
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8',
-    );
+      const markdownWithMeta = fs.readFileSync(
+        path.join('posts', filename),
+        'utf-8',
+      );
 
-    const { data: frontmatter } = matter(markdownWithMeta);
+      const { data: frontmatter } = matter(markdownWithMeta);
 
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+      return {
+        slug,
+        frontmatter,
+      };
+    });
+  }
 
   const results = posts.filter(
     ({ frontmatter: { title, excerpt, category } }) =>
